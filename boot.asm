32f3beb24bdc86be0132a23957f822a6d33941b9
Bogdan0759 <bogdan4ik0759@gmail.com>
2025-01-21 22:04:18 +0200

    Update boot

---
+++ boot.asm
@@ -0,0 +1,20 @@
+[bits 16]
+[org 0x7C00]
+
+mov ah, 0x0E
+mov al, 'H'
+int 0x10
+
+mov al, 'e'
+int 0x10
+
+mov al, 'l'
+int 0x10
+
+mov al, 'l'
+int 0x10
+
+mov al, 'o'
+int 0x10
+
+jmp 0x0000:0x1000
