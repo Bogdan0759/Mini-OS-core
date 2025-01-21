e3d515fb44211a86624da2748a73b20ecf2757c7
Bogdan0759 <bogdan4ik0759@gmail.com>
2025-01-21 22:06:00 +0200

    Update timer

---
+++ rimer.c
@@ -0,0 +1,10 @@
+void setup_timer() {
+    uint32_t divisor = 1193180 / 100;
+    outb(0x43, 0x36);
+    outb(0x40, divisor & 0xFF);
+    outb(0x40, (divisor >> 8) & 0xFF);
+}
+
+void interrupt_handler() {
+   
+}
